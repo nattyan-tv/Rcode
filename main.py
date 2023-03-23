82bd20603e6bf2b5d5f3d07f6cc0886c3cc48167
NattyanTV <73095116+nattyan-tv@users.noreply.github.com>
2023-03-23 11:34:48 +0900

    main.py を更新

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 # Rcode
+import asyncio
+import websockets
+import json
+
+async def serve_handler(websocket, path):
+    async for message in websocket:
+        ctx = json.loads(message)
+        await websocket.send(message)
+
+async def main():
+    async with websockets.serve(serve_handler, "0.0.0.0", 8765):
+        await asyncio.Future()
+ 
+if __name__ == "__main__":
+    asyncio.run(main())
